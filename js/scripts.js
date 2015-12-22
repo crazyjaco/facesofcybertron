dbb4a51cd03997651f0bad0e16804b85209a55e0
crazyjaco <crazyjac0@gmail.com>
2015-12-22 13:38:22 -0500

    Added a close link to collapse about section. Added Google Analytics. Added images for github and twitter links.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,18 @@
 jQuery( document ).ready( function() {
-	// Toggle the about section
-	var toggleAboutSection = function( event ) {
+	// Expand the about section
+	var expandAboutSection = function( event ) {
 		event.preventDefault();
 		if( jQuery('#about').is(':hidden') ) {
 			jQuery('#about').slideDown();
-		} else {
-			jQuery('#about').slideUp();
 		}
 	}
-	jQuery('.about-link').on('click', toggleAboutSection);
+	jQuery('.expand-about').on('click', expandAboutSection);
 
+	// Collapse the about the section
+	var collapseAboutSection = function( event ) {
+		event.preventDefault();
+		jQuery('#about').slideUp();
+	}
+
+	jQuery('.collapse-about').on('click', collapseAboutSection)
 });
